8ac392bf4ca40d1423a00f97524390f06a775f7c
MarcoBz <barison.marco.91@gmail.com>
2018-09-09 08:11:51 +0200

    Add files via upload

--- ico/crowdsale.py
+++ ico/crowdsale.py
@@ -118,12 +118,12 @@ def can_exchange(ctx, attachments, verify_only):
     """
 
     # if you are accepting gas, use this
-#        if attachments[3] == 0:
-#            print("no gas attached")
-#            return False
+   if attachments[3] == 0:
+        print("no gas attached")
+        return False
 
     # if youre accepting neo, use this
-
+ 
     if attachments[2] == 0:
         return False
 
@@ -132,8 +132,8 @@ def can_exchange(ctx, attachments, verify_only):
     # this is not required for operation of the contract
 
 #        status = get_kyc_status(attachments.sender_addr, storage)
-    if not get_kyc_status(ctx, attachments[1]):
-        return False
+#    if not get_kyc_status(ctx, attachments[1]):
+#        return False
 
     # caluclate the amount requested
     amount_requested = attachments[2] * TOKENS_PER_NEO / 100000000
